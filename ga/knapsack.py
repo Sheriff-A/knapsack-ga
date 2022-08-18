9829a59bbcef16fbc8744018f6f2b615e9e24246
Sheriff-A <manshari619@yahoo.com>
2022-08-18 18:37:33 -0400

    implemented bit flip mutation

--- ga/knapsack.py
+++ ga/knapsack.py
@@ -2,6 +2,7 @@ from utils.config import Config
 from typing import List
 
 import numpy as np
+import random
 
 
 def calculate_fitness(chromosome: List[int], bank: List[int], budget: int):
@@ -15,19 +16,27 @@ def calculate_fitness(chromosome: List[int], bank: List[int], budget: int):
 
 
 class Knapsack:
-    def __init__(self, config: Config, bank: List[int], budget: int, chromosome: List[int] = None):
+    def __init__(self, config: Config, chromosome: List[int] = None):
         gene_length = config.get('bank_length')
         if chromosome is None:
             chromosome = np.random.randint(0, 2, gene_length)
 
         self.chromosome = chromosome
+        self.fitness = 0
+
+    # Mutation Algorithm: Bit Flip Mutation
+    def mutate(self):
+        chromosome_length = len(self.chromosome)
+        size = np.random.randint(0, chromosome_length)
+        idx = random.sample(range(chromosome_length), size)
+        for i in idx:
+            self.chromosome[i] = 1 if self.chromosome[i] == 0 else 0
+
+    def get_fitness(self, bank, budget):
         self.fitness = calculate_fitness(self.chromosome, bank, budget)
 
-    def mutate(self):
-        print('Apply Mutation')
-
     def __repr__(self):
-        return f'Solution: {self.chromosome} - Fitness: {self.fitness}'
+        return f'Selection: {self.chromosome} - Fitness: {self.fitness}'
 
     def __lt__(self, other):
         return self.fitness < other.fitness
