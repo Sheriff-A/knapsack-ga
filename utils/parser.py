e5911a47b02a3d28c70cb6ba4ed518ecabfe6a34
Sheriff-A <manshari619@yahoo.com>
2022-08-18 14:33:31 -0400

    added configs for parser

--- utils/parser.py
+++ utils/parser.py
@@ -19,6 +19,61 @@ def command_parser():
         type=int,
         help='Random Initializer Seed'
     )
+    # Minimum Price
+    subps_random.add_argument(
+        '--min_price',
+        type=int,
+        default=100,
+        help="Minimum Value For List of Knapsack Items"
+    )
+
+    # Maximum Price
+    subps_random.add_argument(
+        '--max_price',
+        type=int,
+        default=1000,
+        help="Maximum Value For List of Knapsack Items"
+    )
+
+    # Bank Length
+    subps_random.add_argument(
+        "--bank_length",
+        type=int,
+        default=20,
+        help="Number of Items in the Bank of Items for the Knapsack"
+    )
+
+    # Budget
+    subps_random.add_argument(
+        '--budget',
+        type=int,
+        default=5000,
+        help="Target Value for Knapsack"
+    )
+
+    # Training Parameters
+    # Population Size
+    subps_random.add_argument(
+        '--population_size',
+        type=int,
+        default=8,
+        help="Number of Knapsacks in the Population"
+    )
+
+    # Number of Generations
+    subps_random.add_argument(
+        '--num_generation',
+        type=int,
+        default=8,
+        help="Maximum Number of Generations to Train on"
+    )
+
+    # Show Population
+    subps_random.add_argument(
+        '--show_population',
+        action='store_true',
+        help="Toggle to Show Population after Every Generation"
+    )
 
     subps_random.set_defaults(command='random')
 
